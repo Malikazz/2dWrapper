26e5e028bbf05440d7d0ae4ad2e528cec8424dda
Paul Mana <paul@malikaz.com>
2019-07-28 11:57:35 -0600

    window and point clases

---
+++ Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using SFML.System;
+using SFML.Graphics;
+
+namespace _2dWrapper
+{
+    class Point
+    {
+        public Vector2f PointVector { get; set; }
+        public CircleShape PointShape { get; set; }
+
+
+
+        //Point(int x, int y)
+        //{
+
+        //}
+        Point(float x, float y)
+        {
+            PointVector = new Vector2f(x, y);
+            CircleShape circle = new CircleShape(2);
+            circle.Position = PointVector;
+            circle.FillColor = Color.White;
+            
+        }
+        //Point(FileStream fileOfPoints)
+        //{
+
+        //}
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Window;
+
+namespace _2dWrapper
+{
+    class Window
+    {
+        public void Run()
+        {
+            var mode = new SFML.Window.VideoMode(600, 600);
+            var window = new SFML.Graphics.RenderWindow(mode, "Point Wrapper");
+            window.KeyPressed += Window_KeyPressed;
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                //window.Draw();
+                window.Display();
+            }
+        }
+
+        private void Window_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
+        {
+            var window = (SFML.Window.Window)sender;
+            if(e.Code == SFML.Window.Keyboard.Key.Escape)
+            {
+                window.Close();
+            }
+        }
+        
+
+        
+    }
+}
