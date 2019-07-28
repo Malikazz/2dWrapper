8a8b91b595f829d527fdbf411b53692cc4d87a0c
Paul Mana <paul@malikaz.com>
2019-07-28 11:23:55 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2dWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
