ab5d82b212244c144a48d96a85ad5047dd76bbc4
Paul Mana <paul@malikaz.com>
2019-07-29 15:51:48 -0600

    Random Points now generate, moving onto line and wrapping

--- Program.cs
+++ Program.cs
@@ -4,11 +4,21 @@ namespace _2dWrapper
 {
     class Program
     {
+        static Random rng = new Random();
+
         static void Main(string[] args)
         {
-            
+            Point[] points = new Point[200];
+           
             var window = new Window();
+            for (int counter = 0; counter < 100; counter++)
+            {
+                points[counter] = new Point(rng.Next(20,(int)window.WindowX - 20), rng.Next(20,(int)window.WindowY - 20));
+                window.AddToDraw(points[counter].PointShape);
+            }
             window.Run();
+
+            
         }
     }
 }
--- Window.cs
+++ Window.cs
@@ -7,17 +7,38 @@ namespace _2dWrapper
 {
     public class Window
     {
+        public uint WindowX { get; set; }
+        public uint WindowY { get; set; }
+        public int MAXDRAWABLEOBJECTS { get; set; }
+        SFML.Graphics.Drawable[] drawables;
+
+        private int _indexOfDrawables;
+        public Window(uint windowX = 600, uint windowY = 600, int drawableObjectLimit = 200)
+        {
+            MAXDRAWABLEOBJECTS = drawableObjectLimit;
+            drawables = new SFML.Graphics.Drawable[MAXDRAWABLEOBJECTS];
+            WindowX = windowX;
+            WindowY = windowY;
+        }
+
+
         Point point = new Point(200, 200);
         public void Run()
         {
-            var mode = new SFML.Window.VideoMode(600, 600);
+            var mode = new SFML.Window.VideoMode(WindowX, WindowY);
             var window = new SFML.Graphics.RenderWindow(mode, "Point Wrapper");
             window.KeyPressed += Window_KeyPressed;
 
             while (window.IsOpen)
             {
                 window.DispatchEvents();
-                window.Draw(point.PointShape);
+                for(int counter = 0; counter < drawables.Length; counter++)
+                {
+                    if (drawables[counter] != null)
+                    {
+                        window.Draw(drawables[counter]);
+                    }
+                }
                 window.Display();
             }
         }
@@ -32,6 +53,15 @@ namespace _2dWrapper
         }
         
 
-        
+        /// <summary>
+        /// This will need to be changed if you want drawables to be mutable,
+        /// as there is not way to edit the array accuratly besides restarting.
+        /// </summary>
+        /// <param name="shapeToDraw"></param>
+        public void AddToDraw(SFML.Graphics.Drawable shapeToDraw)
+        {
+            drawables[_indexOfDrawables] = shapeToDraw;
+            _indexOfDrawables++;
+        }
     }
 }
