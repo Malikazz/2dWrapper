f0eb24121cbdd45bc8fa7d4a4259d8a4a430bde6
Paul Mana <paul@malikaz.com>
2019-07-30 18:37:37 -0600

    need to finish solving for rest of angle possibilities

--- Algorithms.cs
+++ Algorithms.cs
@@ -11,8 +11,10 @@ namespace _2dWrapper
 
         }
 
-        public double FindInternalAnglesOfObliqueTriangle(double sideA, double sideB, double sideC)
+        public double[] FindInternalAnglesOfObliqueTriangle(double sideA, double sideB, double sideC)
         {
+            // a / b / c
+            double[] angles = new double[3];
             //c2 = a2 +b2 - 2ab cosC
             double _2ab;
             double a2b2;
@@ -20,12 +22,16 @@ namespace _2dWrapper
             double a2;
             double b2;
             double c2;
+            double sinA;
+            double sinB;
+            double sinC;
+            a2 = sideA * sideA;
+            b2 = sideB * sideB;
+            c2 = sideC * sideC;
 
             if (sideC > sideB && sideC > sideA)
             {
-                a2 = sideA * sideA;
-                b2 = sideB * sideB;
-                c2 = sideC * sideC;
+
 
                 a2b2 = a2 + b2;
                 _2ab = (sideA * 2) * sideB;
@@ -38,15 +44,46 @@ namespace _2dWrapper
                 //left with c2 = cosC
                 cosC = (Math.Acos(c2) * 180 / Math.PI);
                 //c2 is at final angle;
-                return cosC;
+                angles[2] = cosC;
+                sinA = Math.Asin((Math.Sin(cosC) * sideA) / sideC);
+                angles[0] = sinA;
+                angles[1] = 180 - (angles[0] + angles[2]);
+                if (angles[0] + angles[1] + angles[2] > 180)
+                {
+                    Console.WriteLine("Angles added upto over 180 error");
+                }
+                return angles;
+
             }
             else if(sideB > sideC && sideB > sideA)
             {
-                return 0.0;
+                a2b2 = a2 + c2;
+                _2ab = (sideA * 2) * sideC;
+                _2ab *= -1;
+
+                //takes out the a2b2
+                c2 -= a2b2;
+                //takes out the _2ab
+                c2 /= _2ab;
+                //left with c2 = cosC
+                cosC = (Math.Acos(c2) * 180 / Math.PI);
+                //c2 is at final angle;
+                angles[1] = cosC;
             }
             else
             {
-                return 0.0;
+                a2b2 = c2 + b2;
+                _2ab = (sideC * 2) * sideB;
+                _2ab *= -1;
+
+                //takes out the a2b2
+                c2 -= a2b2;
+                //takes out the _2ab
+                c2 /= _2ab;
+                //left with c2 = cosC
+                cosC = (Math.Acos(c2) * 180 / Math.PI);
+                //c2 is at final angle;
+                angles[0] = cosC;
             }
         }
     }
